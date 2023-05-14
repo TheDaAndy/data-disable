085941123cfc9ae32da4cc37c81a9a0ec975aa55
Andreas Ullrich <au@elunic.com>
2023-05-14 19:08:40 +0200

    init first version

--- app/src/main/java/com/example/disabledata/MainActivity.kt
+++ app/src/main/java/com/example/disabledata/MainActivity.kt
@@ -1,42 +1,65 @@
+package com.example.disabledata
+
+import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.widget.EditText
 import android.widget.Switch
 import androidx.activity.ComponentActivity
-import androidx.activity.viewModels
-import com.example.disabledata.R
 
 class MainActivity : ComponentActivity() {
 
-    private lateinit var serviceSwitch: Switch
+    private lateinit var mobileDataSwitch: Switch
+    private lateinit var wlanSwitch: Switch
     private lateinit var restoreSettingsSwitch: Switch
-    private lateinit var delayInput: EditText
+    private lateinit var delayTimeInput: EditText
+
+    private var checkingForRoot = false
+
+    private val sharedPreferencesListener = SharedPreferences.OnSharedPreferenceChangeListener { sharedPreferences, key ->
+        if (key == "rootCheckSuccessful" && !sharedPreferences.getBoolean("rootCheckSuccessful", false)) {
+            checkForRoot()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        serviceSwitch = findViewById(R.id.serviceSwitch)
+        mobileDataSwitch = findViewById(R.id.serviceSwitchMobileData)
+        wlanSwitch = findViewById(R.id.serviceSwitchWLAN)
         restoreSettingsSwitch = findViewById(R.id.restoreSettingsSwitch)
-        delayInput = findViewById(R.id.delayInput)
+
+        // Retrieve the initial value from shared preferences
+        val prefs = getSharedPreferences("de.thedaandy.data_disabler", Context.MODE_PRIVATE)
+        val initialDelayTime = prefs.getLong("delayTime", 0)
+        // Initialize the delay time input and set the initial value
+        delayTimeInput = findViewById(R.id.delayTimeInput)
+        delayTimeInput.setText(initialDelayTime.toString())
 
         // Restore previous switch states
-        val prefs = getSharedPreferences("MyPrefs", MODE_PRIVATE)
-        serviceSwitch.isChecked = prefs.getBoolean("serviceEnabled", false)
+        wlanSwitch.isChecked = prefs.getBoolean("wlanDisablerEnabled", false)
+        mobileDataSwitch.isChecked = prefs.getBoolean("mobileDataDisablerEnabled", false)
         restoreSettingsSwitch.isChecked = prefs.getBoolean("restoreSettingsEnabled", false)
-        delayInput.setText(prefs.getString("delay", ""))
-
-        serviceSwitch.setOnCheckedChangeListener { _, isChecked ->
+        wlanSwitch.setOnCheckedChangeListener { _, isChecked ->
             val editor = prefs.edit()
-            editor.putBoolean("serviceEnabled", isChecked)
+            editor.putBoolean("wlanDisablerEnabled", isChecked)
             editor.apply()
+            val intent = Intent(this, DataDisablerService::class.java)
+            startService(intent)
+        }
 
-            val intent = Intent(this, MyService::class.java)
-            if (isChecked) {
-                startService(intent)
-            } else {
-                stopService(intent)
-            }
+        mobileDataSwitch.setOnCheckedChangeListener { _, isChecked ->
+            val editor = prefs.edit()
+            editor.putBoolean("mobileDataDisablerEnabled", isChecked)
+            editor.apply()
+            val intent = Intent(this, DataDisablerService::class.java)
+            startService(intent)
         }
 
         restoreSettingsSwitch.setOnCheckedChangeListener { _, isChecked ->
@@ -45,12 +68,97 @@ class MainActivity : ComponentActivity() {
             editor.apply()
         }
 
-        delayInput.setOnFocusChangeListener { _, hasFocus ->
-            if (!hasFocus) {
+        // Create a TextWatcher to detect changes in the input field
+        val delayTimeWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Not needed for this case
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Save the input value when it changes
                 val editor = prefs.edit()
-                editor.putString("delay", delayInput.text.toString())
+                editor.putLong("delayTime", s.toString().toLongOrNull() ?: 0)
                 editor.apply()
             }
+
+            override fun afterTextChanged(s: Editable?) {
+                // Not needed for this case
+            }
         }
+
+        // Set the TextWatcher on the input field
+        delayTimeInput.addTextChangedListener(delayTimeWatcher)
+
+        checkForRoot()
+
+        // Register the SharedPreferences listener
+        prefs.registerOnSharedPreferenceChangeListener(sharedPreferencesListener)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        checkForRoot()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        // Unregister the SharedPreferences listener to avoid memory leaks
+        val prefs = getSharedPreferences("de.thedaandy.data_disabler", Context.MODE_PRIVATE)
+        prefs.unregisterOnSharedPreferenceChangeListener(sharedPreferencesListener)
+    }
+
+
+    private fun checkForRoot() {
+        if (checkingForRoot) {
+            return
+        }
+        val prefs = getSharedPreferences("de.thedaandy.data_disabler", Context.MODE_PRIVATE)
+        if (!prefs.getBoolean("rootCheckSuccessful", false)) {
+            checkingForRoot = true
+            val alertDialogBuilder = AlertDialog.Builder(this)
+            alertDialogBuilder.setTitle("Requesting Root Access")
+            alertDialogBuilder.setMessage("Root access is required to disable/enable mobile data and WLAN. Please grant root access to this app for the following request.")
+            alertDialogBuilder.setPositiveButton("OK") { dialog, _ ->
+                dialog.dismiss()
+                if (DataDisablerService.rootAvailable()) {
+                    val edit = prefs.edit()
+                    edit.putBoolean("rootCheckSuccessful", true)
+                    edit.apply()
+                    checkingForRoot = false
+                } else {
+                    showNoRootPopup()
+                    checkingForRoot = false
+                }
+            }
+
+            val alertDialog = alertDialogBuilder.create()
+            alertDialog.setCancelable(false)
+            alertDialog.show()
+        } else {
+            onSuccessfulRootAccessCheck()
+        }
+    }
+
+    private fun onSuccessfulRootAccessCheck() {
+        try {
+            val intent = Intent(this, DataDisablerService::class.java)
+            startService(intent)
+        } catch (e: Exception) {
+            Log.d("MainActivity", "Could not start DataDisablerService")
+        }
+    }
+
+    private fun showNoRootPopup() {
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        alertDialogBuilder.setTitle("Root Access Required")
+        alertDialogBuilder.setMessage("Root access is required to use this app. Please make sure your device is rooted. Closing App now.")
+        alertDialogBuilder.setPositiveButton("OK") { dialog, _ ->
+            dialog.dismiss()
+            finish()
+        }
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.setCancelable(false)
+        alertDialog.show()
     }
 }
